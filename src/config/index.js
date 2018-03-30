341404e031e6a4281788e587aaa5de9a9e4fc492
zhangji <1070238361@qq.com>
2018-03-30 18:16:20 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ let routerMode = 'history'
 let imgBaseUrl = 'http://images.cangdu.org/'
 const KEY = "ZND20171030APIMM"
 if (process.env.NODE_ENV === 'development') {
-    // baseUrl = 'http://wuhanxingrong.vicp.io:18887/'
-    // baseUrl2 = 'http://wuhanxingrong.vicp.io:8185/'
+    baseUrl = 'http://wuhanxingrong.vicp.io:18887/'
+    baseUrl2 = 'http://wuhanxingrong.vicp.io:8185/'
     // baseUrl = 'http://192.168.0.31:8185/'
     // baseUrl2 = 'http://192.168.0.31:8185/'
-    baseUrl = 'http://192.168.0.42:8887/'
-    baseUrl2 = 'http://192.168.0.42:8887/'
+    // baseUrl = 'http://192.168.0.42:8887/'
+    // baseUrl2 = 'http://192.168.0.42:8887/'
     // baseUrl = '192.168.0.42:8887/'//金融超市地址
     // baseUrl2 = ''//OA地址
     // baseUrl = 'http://tdx.free.ngrok.cc/zndai/'
