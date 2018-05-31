5633c877c2e3f869860876b238b441110af2176e
1004606010@qq.com <mingxin@istarfinancial.cn>
2018-05-31 16:28:16 +0800

    update user

--- src/pages/users/myService.vue
+++ src/pages/users/myService.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="myService">
-    <v-header class="header">
-			<i slot="left" class="el-icon-arrow-left"></i>
-			<p slot="title">在线客服</p>
-      <!-- <i slot="right"><img src="../../assets/images/service.png" alt="" @click="pop"></i> -->
-		</v-header>
+    <v-header title="在线客服"></v-header>
     <div class="open" v-show="openService">
       <div  class="openBox">
       <div class="imgWrap"><img src="../../assets/images/pop.png" alt=""></div>
@@ -32,12 +28,9 @@
     </ul>
     <ul class="footer">
         <!-- <li class="img1"><img src="../../assets/images/face.png" alt=""></li> -->
-        <li class="in1"><textarea type="text" id="msg"></textarea></li>
+        <li class="in1"><input type="text" class="customerMsg" id="msg" placeholder="请输入你想要咨询的问题" /></li>
         <li class="btn" @click="sendMessage()">
-            <el-tooltip :disabled="disabled" content="不能发送空白信息" placement="bottom" effect="light">
-                <el-button @click="disabled = !disabled">发送</el-button>
-                <input type="button" value="发送">
-            </el-tooltip>
+            <input type="button" value="发送">
         </li>
     </ul>
   </div>
@@ -57,7 +50,8 @@ export default {
       selected: "xiaoxiao",
       Emoji: WebIM.Emoji,
       userInfo: JSON.parse(getItem("userInfo")),
-      tokenkey: ""
+      tokenkey: "",
+      show: false
     };
   },
   mounted() {
@@ -131,9 +125,9 @@ export default {
       var data = {
         userId: this.userInfo.userId
       };
-      data = strEnc(JSON.stringify(data), "ZND20171030APIMM");
+      //   data = strEnc(JSON.stringify(data), "ZND20171030APIMM");
       let res = await getToken(data);
-      res.data = JSON.parse(strDec(res.data, "ZND20171030APIMM"));
+      //   res.data = JSON.parse(strDec(res.data, "ZND20171030APIMM"));
       if (res.code === "0000") {
         this.tokenkey = res.data.access_token;
         // console.log("aaaa", this.tokenkey, res.data.access_token);
@@ -223,7 +217,8 @@ export default {
     goBottom() {
       var goBottomDiv = document.getElementsByClassName("chat")[0];
       goBottomDiv.scrollTop = goBottomDiv.scrollHeight;
-    }
+    },
+    consoleCode() {}
   }
 };
 </script>
@@ -318,10 +313,10 @@ export default {
 
   .footer {
     width: 100%;
-    background-color: #f8f8f8;
+    height: rem(120px);
+    background-color: #fff;
     position: fixed;
-    bottom: rem(20px);
-    height: rem(100px);
+    bottom: 0;
     li:nth-child(1) {
       float: left;
       margin-left: rem(30px);
@@ -346,24 +341,22 @@ export default {
     }
     .in1 {
       width: 67%;
-      height: rem(70px);
-      margin-top: rem(14px);
-      textarea {
-        // width: rem(450px);
+      height: rem(100px);
+      margin-top: rem(22px);
+      .customerMsg {
         width: 100%;
-        // height: rem(70px);
-        // border: 0;
-        background-color: #fff;
-        border-radius: 5px;
+        height: rem(80px);
+        // border: 1px solid #000;
+        background-color: #fcfafa;
+        border-radius: rem(40px);
         font-size: rem(30px);
-        line-height: rem(24px);
-        padding: rem(15px);
+        padding-left: rem(30px);
       }
     }
     .btn {
       width: 21%;
       height: rem(68px);
-      margin-top: rem(16px);
+      margin-top: rem(25px);
 
       //  margin-right: rem(30px);
       input {
